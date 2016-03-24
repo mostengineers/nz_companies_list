bc91af0ddaac5d37cee76b80f61bdcf08a75a607
MOST <admin@mostengineers.com>
2016-03-25 11:57:27 +1300

    output to CSV

--- get_company_info.rb
+++ get_company_info.rb
@@ -4,14 +4,24 @@ require 'pry'
 
 search_url = 'https://www.business.govt.nz/companies/app/ui/pages/companies/search?q=&entityTypes=LTD&entityStatusGroups=REGISTERED&incorpFrom=01%2F03%2F2016&incorpTo=01%2F03%2F2016&addressTypes=ALL&addressKeyword=&start=0&limit=200&sf=&sd=&advancedPanel=true&mode=advanced#results'
 
-doc = Nokogiri::HTML(open(search_url))
+company_details_url = 'https://www.business.govt.nz/companies/app/ui/pages/companies/' # + 5897810 to get details page
 
-doc.css('.dataList tr').each do |company_data|
+harvested_results = []
+
+company_search_results = Nokogiri::HTML(open(search_url))
+
+company_search_results.css('.dataList tr').each do |company_data|
   if company_data.css('.link')[0]
-    puts company_data.css('.link')[0][:href] 
-    puts company_data.css('.entityName')
-    puts company_data.css('.entityInfo')
-    puts company_data.css('.incorporationDate label')
+    popup_link = company_data.css('.link')[0][:href] 
+    company_id = popup_link.scan(/'([^']*)'/)
+    company_details = [
+      company_id,
+      company_data.css('.entityName').text,
+      #company_data.css('.entityInfo').text,
+      company_data.css('.incorporationDate label').text
+    ] 
+    harvested_results <<  company_details.join(',')
   end
 end
 
+puts harvested_results
